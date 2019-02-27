944ff3987ac3e3c0cb35aadfe2bea4b3c0d8fbca
wangxiaochuan <wangxiaochuan@shein.com>
2019-02-27 09:34:56 +0800

    init project

---
+++ src/main/java/com/xiaochuan/apollosample/ApollosampleApplication.java
@@ -0,0 +1,13 @@
+package com.xiaochuan.apollosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApollosampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApollosampleApplication.class, args);
+    }
+
+}
